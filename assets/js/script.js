89293de3992529c71a31f13664092f878f1bda91
Chris Southwick <csouthwick@live.com>
2020-09-06 21:04:28 -0400

    start of save click handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,17 @@ var loadSchedule = function () {
   $("#currentDay").text(today.format("dddd, MMMM Do"));
 }
 
+// save button was clicked
+$(document).on("click", ".saveBtn", function () {
+  var timeBlock = $(this).closest(".time-block");
+  var hour = timeBlock.find(".hour div").text().trim();
+  var description = timeBlock.find(".description").text().trim();
+  var data = {
+    hour: hour,
+    description: description
+  };
+  console.log(data);
+});
+
+
 loadSchedule();
