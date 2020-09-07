743d01ebb27d1cfa3a69583546b9083ef6bde20a
Chris Southwick <csouthwick@live.com>
2020-09-06 21:54:25 -0400

    add persistance with localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 var loadSchedule = function () {
   var today = moment();
   $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+  $(".time-block").each(function () {
+    var hour = $(this).find(".hour div").text().trim();
+    var description = localStorage.getItem(hour) || "";
+    $(this).find(".description").text(description);
+  });
 }
 
 // task description was clicked
@@ -37,11 +43,7 @@ $(document).on("click", ".saveBtn", function () {
   var timeBlock = $(this).closest(".time-block");
   var hour = timeBlock.find(".hour div").text().trim();
   var description = timeBlock.find(".description").text().trim();
-  var data = {
-    hour: hour,
-    description: description
-  };
-  console.log(data);
+  localStorage.setItem(hour, description);
 });
 
 
