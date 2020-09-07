4ac0a9d22854828412eb7b95814bcef70e24efec
Chris Southwick <csouthwick@live.com>
2020-09-06 21:32:44 -0400

    make the descriptions editable on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,35 @@ var loadSchedule = function () {
   $("#currentDay").text(today.format("dddd, MMMM Do"));
 }
 
+// task description was clicked
+$(document).on("click", ".description", function () {
+  // get current text
+  var text = $(this).text().trim();
+
+  // get current classes
+  var classes = $(this).attr("class");
+
+  // replace with a new textarea
+  var textInput = $("<textarea>").addClass(classes).val(text);
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
+
+// textarea lost focus
+$(document).on("blur", "textarea.description", function () {
+  // get current value
+  var text = $(this).val().trim();
+
+  // get current classes
+  var classes = $(this).attr("class");
+
+  // replace with a new div
+  var div = $("<div>").addClass(classes).text(text);
+  $(this).replaceWith(div);
+});
+
 // save button was clicked
 $(document).on("click", ".saveBtn", function () {
   var timeBlock = $(this).closest(".time-block");
